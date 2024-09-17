aa1480b83b8e752a5eecbb457bf2180e0fa6a5fd
João Victor Pimentel <joaovitorcavalcante009@gmail.com>
2024-09-17 08:52:47 -0300

    .

--- resources/js/components/login/LoginComponent.vue
+++ resources/js/components/login/LoginComponent.vue
@@ -28,19 +28,6 @@
                                         v-model="password">
                                 </div>
                             </div>
-
-                            <div class="row mb-3">
-                                <div class="col-md-6 offset-md-4">
-                                    <div class="form-check">
-                                        <input class="form-check-input" type="checkbox" name="remember" id="remember">
-
-                                        <label class="form-check-label" for="remember">
-                                            Matenha-me conectado
-                                        </label>
-                                    </div>
-                                </div>
-                            </div>
-
                             <div class="row mb-0">
                                 <div class="col-md-8 offset-md-4">
                                     <button type="submit" class="btn btn-secondary">
